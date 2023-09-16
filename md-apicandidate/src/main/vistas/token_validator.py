1f55e4fe78811f2c34a33fec1104f3f1092e5602
Luis Solier Sajami <luis.sajami@gmail.com>
2023-09-16 12:11:53 -0500

    Se envia token por header desde mp-apicandidate

--- md-apicandidate/src/main/vistas/token_validator.py
+++ md-apicandidate/src/main/vistas/token_validator.py
@@ -14,15 +14,9 @@ class TokenValidator:
             if Config.DENY_ACCESS:
                 print('Access denied, API_SECURITY_URL does not exist')
                 return '', requests.codes.unauthorized
-            try:
-                token_header = request.headers['authorization']
-                auth_token = token_header.split(maxsplit=1)[1]
-            except:
-                print("Access denied, an exception occurred reading the token, verify authorization header")
-                return '', requests.codes.unauthorized
-            print('TOKEN: ' + auth_token)
-            __payload = {'token': auth_token}
-            response = requests.get(Config.API_SECURITY_URL, params=__payload, verify=False)
+            token_header = request.headers['authorization']
+            print('TOKEN: ' + token_header)
+            response = requests.get(Config.API_SECURITY_URL, headers={"Authorization": token_header, "Content-Type": "application/json"})
             if response.status_code != requests.codes.ok:
                 print('Access denied, token is invalid')
                 return '', requests.codes.unauthorized
