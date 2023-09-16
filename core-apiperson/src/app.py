5d3e64fd83cf35158ae81ae1b997116847196a18
Luis Solier Sajami <luis.sajami@gmail.com>
2023-09-16 14:54:20 -0500

    Se agrega refactoring

--- core-apiperson/src/app.py
+++ core-apiperson/src/app.py
@@ -2,22 +2,24 @@ from src import create_app
 import logging
 from flask_restful import Api
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager
 from src.main.vistas import HealthView, PersonsView, PersonView
 from src.main.modelos import db
 
+
+# app setup
 app = create_app(__name__)
 logging.basicConfig(level=logging.DEBUG)
 app_context = app.app_context()
 app_context.push()
+# db setup
 db.init_app(app)
 db.create_all()
+# Cors
 cors = CORS(app)
 
+# endpoints definition
 api = Api(app)
 api.add_resource(PersonsView,'/persons')
 api.add_resource(HealthView,'/persons/ping')
 api.add_resource(PersonView,'/persons/<id>')
-
-jwt = JWTManager(app)
 
--- core-apiperson/src/config.py
+++ core-apiperson/src/config.py
@@ -1,7 +1,5 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL_USERS", "sqlite:///test-core-apiperson.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
